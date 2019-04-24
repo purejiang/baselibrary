4d573fef6cac4d02aaa3feec66bc1c5927d15f2f
JiangCodePlus <m18569290458@163.com>
2019-04-24 20:13:31 +0800

    NiceAlertDialog支持RecyclerView作为list的布局

--- baselibrary/src/main/java/com/nice/baselibrary/base/ui/view/dialog/NiceAlertDialog.kt
+++ baselibrary/src/main/java/com/nice/baselibrary/base/ui/view/dialog/NiceAlertDialog.kt
@@ -8,23 +8,28 @@ import android.support.v4.app.FragmentTransaction
 import android.util.Log
 import android.view.View
 import com.nice.baselibrary.base.adapter.NiceAdapter
+import android.R
+import android.hardware.SensorManager.getOrientation
+import android.support.v7.widget.RecyclerView
+import com.leniu.assist.ln.entity.AppInfo
+import android.support.v7.widget.LinearLayoutManager
+
+
 
 
 /**
+ * 自定义DialogFragment
  * @author JPlus
  * @date 2019/4/17.
  */
 
-class NiceAlertDialog : NiceDialogFragment() {
+open class NiceAlertDialog : NiceDialogFragment() {
     private var mController: NiceDialogController? = null
 
     init {
         mController = NiceDialogController.create()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun bindView(view: View) {
         val viewHolder = NiceAdapter.VH(view)
@@ -36,6 +41,17 @@ class NiceAlertDialog : NiceDialogFragment() {
             }
             }
         this.getBindViewListener()?.onBindView(viewHolder)
+
+        //adapter不为空则设置adapter和recyclerView
+        getListAdapter()?.let{
+            it.setItemClickListener(getListItemClickListener())
+            val layoutManager = LinearLayoutManager(view.context, getListOreation(), false)
+            view.findViewById<RecyclerView>(getListRecyclerId()).run{
+                setLayoutManager(layoutManager)
+                adapter = getListAdapter()
+            }
+            it.notifyDataSetChanged()
+        }
 
     }
 
@@ -74,15 +90,30 @@ class NiceAlertDialog : NiceDialogFragment() {
         return mController!!.getBackgroundRes()
     }
 
-    private fun getViewClickListener(): NiceAlertDialog.OnViewClickListener? {
-        return mController!!.getOnViewClickListener()
-    }
+
     private fun getBindViewListener(): NiceAlertDialog.OnBindViewListener? {
         return mController!!.getOnBindViewListener()
     }
+    private fun getViewClickListener(): NiceAlertDialog.OnViewClickListener? {
+        return mController!!.getOnViewClickListener()
+    }
 
+    private fun getListItemClickListener(): NiceAdapter.ItemClickListener? {
+        return mController!!.getListItemClickListener()
+    }
 
-    fun show(): NiceAlertDialog {
+    private fun getListAdapter():NiceAdapter<*>?{
+        return mController!!.getAdapter()
+    }
+
+    private fun getListRecyclerId():Int{
+        return mController!!.getListRecyclerId()
+    }
+    private fun getListOreation():Int{
+        return mController!!.getListOrientation()
+    }
+
+    open fun show(): NiceAlertDialog {
         try {
             mController?.getFragmentTransaction()?.let {
                 it.add(this, mController?.getTag())
@@ -102,7 +133,6 @@ class NiceAlertDialog : NiceDialogFragment() {
         fun onBindView(viewHolder: NiceAdapter.VH)
     }
 
-
     class Builder(mFragmentTransaction: FragmentTransaction) {
 
         private var params: NiceDialogController.Params? = null
@@ -112,78 +142,176 @@ class NiceAlertDialog : NiceDialogFragment() {
             params?.mFragmentTransaction = mFragmentTransaction
         }
 
+        /**
+         * 设置布局
+         * @param layoutRes
+         * @return Builder
+         */
         fun setLayoutRes(layoutRes: Int): Builder {
             params?.mLayoutRes = layoutRes
             return this
         }
-
+        /**
+         * 设置dialog所处位置
+         * @param gravity
+         * @return Builder
+         */
         fun setGravity(gravity: Int): Builder {
             params?.mGravity = gravity
             return this
         }
-
+        /**
+         * 设置是否可取消
+         * @param isCancelable
+         * @return Builder
+         */
         fun setCancelable(isCancelable: Boolean): Builder {
             params?.mIsCancelable = isCancelable
             return this
         }
-
+        /**
+         * 设置Tag
+         * @param tag
+         * @return Builder
+         */
         fun setTag(tag: String): Builder {
             params?.mTag = tag
             return this
         }
-
+        /**
+         * 设置动画
+         * @param animationRes
+         * @return Builder
+         */
         fun setAnimationRes(animationRes: Int): Builder {
             params?.mAnimationRes = animationRes
             return this
         }
-
+        /**
+         * 设置百分比宽
+         * @param context 上下文
+         * @param percent
+         * @return Builder
+         */
         fun setScreenWidthPercent(context: Context, percent: Float): Builder {
             params?.mDialogWidth = (getScreenWidth(context) * percent).toInt()
             return this
         }
-
+        /**
+         * 设置百分比高
+         * @param context 上下文
+         * @param percent
+         * @return Builder
+         */
         fun setScreenHeightPercent(context: Context, percent: Float): Builder {
             params?.mDialogHeight = (getScreenHeight(context) * percent).toInt()
             return this
         }
+        /**
+         * 设置dialog布局背景
+         * @param backgroundRes
+         * @return Builder
+         */
         fun setBackgroundRes(backgroundRes: Int): Builder {
             params?.mBackgroundRes = backgroundRes
             return this
         }
-
+        /**
+         * 设置dialog高度
+         * @param height
+         * @return Builder
+         */
         fun setDialogHeight(height: Int): Builder {
             params?.mDialogHeight = height
             return this
         }
-
+        /**
+         * 设置dialog宽度
+         * @param width
+         * @return Builder
+         */
         fun setDialogWidth(width: Int): Builder {
             params?.mDialogWidth = width
             return this
         }
-
+        /**
+         * 设置dialog阴影透明度
+         * @param amount
+         * @return Builder
+         */
         fun setDimAmount(amount: Float): Builder {
             params?.mDimAmount = amount
             return this
         }
-
+        /**
+         * 设置按键监听
+         * @param onKeyListener
+         * @return Builder
+         */
         fun setKeyListener(onKeyListener: DialogInterface.OnKeyListener): Builder {
             params?.mOnKeyListener = onKeyListener
             return this
         }
-
+        /**
+         * 添加可点击事件控件
+         * @param ids
+         * @return Builder
+         */
         fun addClickedId(vararg ids: Int): Builder {
             params?.mIds = ids
             return this
         }
-
+        /**
+         * 设置控件点击事件
+         * @param clickListener
+         * @return Builder
+         */
         fun setViewClickListener(clickListener: NiceAlertDialog.OnViewClickListener): Builder {
             params?.mOnViewClickListener = clickListener
             return this
         }
+        /**
+         * 设置列表条目点击事件
+         * @param clickListener
+         * @return Builder
+         */
+        fun setListItemClickListener(clickListener: NiceAdapter.ItemClickListener): Builder {
+            params?.mListItemClickListener = clickListener
+            return this
+        }
+        /**
+         * 设置控件的其他方法监听
+         * @param bindViewListener
+         * @return Builder
+         */
         fun setBindViewListener(bindViewListener: NiceAlertDialog.OnBindViewListener): Builder {
             params?.mOnBindViewListener = bindViewListener
             return this
         }
+        /**
+         * 设置列表recyclerId和滚动方向
+         * @param recyclerId
+         * @param orientation
+         * @return Builder
+         */
+        fun setListRes(recyclerId:Int, orientation:Int):Builder {
+            params?.mListRecyclerId =recyclerId
+            params?.mListOrientation = orientation
+            return this
+        }
+        /**
+         * 设置适配器
+         * @param adapter
+         * @return Builder
+         */
+        fun setAdapter(adapter: NiceAdapter<*>):Builder {
+            params?.mAdapter = adapter
+            return this
+        }
+        /**
+         * 创建NiceDialog
+         * @return NiceAlertDialog
+         */
 
         fun create(): NiceAlertDialog {
             val dialog = NiceAlertDialog()
@@ -192,7 +320,5 @@ class NiceAlertDialog : NiceDialogFragment() {
             return dialog
         }
     }
-
-
 
 }
--- baselibrary/src/main/java/com/nice/baselibrary/base/ui/view/dialog/NiceDialogController.kt
+++ baselibrary/src/main/java/com/nice/baselibrary/base/ui/view/dialog/NiceDialogController.kt
@@ -1,13 +1,13 @@
 package com.nice.baselibrary.base.ui.view.dialog
 
 
-
 import android.content.DialogInterface
 import android.support.v4.app.FragmentTransaction
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
 import com.nice.baselibrary.R
+import com.nice.baselibrary.base.adapter.NiceAdapter
 
 /**
  * 自定义dialog的工具类
@@ -22,23 +22,32 @@ class NiceDialogController private constructor() {
     }
 
     private var mFragmentTransaction: FragmentTransaction? = null
-    private var mLayoutRes: Int = 0
-    private var mBackgroundRes :Int =0
-    private var mDialogHeight: Int = 0
-    private var mDialogWidth: Int = 0
-    private var mDimAmount: Float = 0.0f
-    private var mGravity: Int = 0
-    private var mTag: String = ""
+    private var mLayoutRes = 0
+    private var mBackgroundRes = 0
+    private var mDialogHeight = 0
+    private var mDialogWidth = 0
+    private var mDimAmount = 0.0f
+    private var mGravity = 0
+    private var mTag = ""
     private var mIds: IntArray? = null
-    private var mIsCancelable: Boolean = false
-    private var mOrientation: Int = 0
-    private var mAnimationRes: Int = 0
+    private var mIsCancelable = false
+    private var mOrientation = 0
+    private var mAnimationRes = 0
     private var mDialogView: View? = null
     private var mOnViewClickListener: NiceAlertDialog.OnViewClickListener? = null
-    private var mOnBindViewListener:NiceAlertDialog.OnBindViewListener?=null
+    private var mOnBindViewListener: NiceAlertDialog.OnBindViewListener? = null
     private var mOnDismissListener: DialogInterface.OnDismissListener? = null
     private var mOnKeyListener: DialogInterface.OnKeyListener? = null
 
+
+    private var mAdapter: NiceAdapter<*>? = null
+    private var mListItemClickListener: NiceAdapter.ItemClickListener? = null
+    private var mListRecyclerId = 0
+    private var mListOrientation = 0
+
+    /*
+       Dialog通常用到的方法
+    */
     fun getFragmentTransaction(): FragmentTransaction? {
         return mFragmentTransaction
     }
@@ -47,7 +56,7 @@ class NiceDialogController private constructor() {
         return mLayoutRes
     }
 
-    fun getTag():String{
+    fun getTag(): String {
         return mTag
     }
 
@@ -74,6 +83,7 @@ class NiceDialogController private constructor() {
     fun getDimAmount(): Float {
         return mDimAmount
     }
+
     fun getBackgroundRes(): Int {
         return mBackgroundRes
     }
@@ -81,38 +91,69 @@ class NiceDialogController private constructor() {
     fun getKeyListener(): DialogInterface.OnKeyListener? {
         return mOnKeyListener
     }
+
     fun getIds(): IntArray? {
         return mIds
     }
 
-    fun getOnViewClickListener():NiceAlertDialog.OnViewClickListener?{
+    /*
+       普通Dialog用到的方法
+    */
+    fun getOnViewClickListener(): NiceAlertDialog.OnViewClickListener? {
         return mOnViewClickListener
     }
-    fun getOnBindViewListener():NiceAlertDialog.OnBindViewListener?{
+
+    fun getOnBindViewListener(): NiceAlertDialog.OnBindViewListener? {
         return mOnBindViewListener
+    }
+
+    /*
+        列表Dialog用到的方法
+    */
+
+    fun getAdapter(): NiceAdapter<*>? {
+        return mAdapter
+    }
+
+    fun getListItemClickListener(): NiceAdapter.ItemClickListener? {
+        return mListItemClickListener
+    }
+
+
+    fun getListRecyclerId(): Int {
+        return mListRecyclerId
+    }
+
+    fun getListOrientation(): Int {
+        return mListOrientation
     }
 
     /**
      * 自定义dialog的参数类
      */
     class Params {
-         var mFragmentTransaction: FragmentTransaction? = null
-         var mLayoutRes = R.layout.view_dialog
-         var mBackgroundRes :Int = R.drawable.bg_circle_view
-         var mDialogHeight: Int = WindowManager.LayoutParams.WRAP_CONTENT
-         var mDialogWidth: Int = WindowManager.LayoutParams.WRAP_CONTENT
-         var mDimAmount: Float = 0.0f
-         var mGravity: Int = Gravity.CENTER
-         var mTag: String = ""
-         var mIds: IntArray? = null
-         var mIsCancelable: Boolean = false
-         var mOrientation: Int = 0
-         var mAnimationRes: Int = R.style.NiceDialogAnim
-         var mDialogView: View? = null
-         var mOnViewClickListener: NiceAlertDialog.OnViewClickListener? = null
-         var mOnBindViewListener: NiceAlertDialog.OnBindViewListener? = null
-         var mOnDismissListener: DialogInterface.OnDismissListener? = null
-         var mOnKeyListener: DialogInterface.OnKeyListener? = null
+        var mFragmentTransaction: FragmentTransaction? = null
+        var mLayoutRes = R.layout.view_dialog
+        var mBackgroundRes = R.drawable.bg_circle_view
+        var mDialogHeight = WindowManager.LayoutParams.WRAP_CONTENT
+        var mDialogWidth = WindowManager.LayoutParams.WRAP_CONTENT
+        var mDimAmount = 0.0f
+        var mGravity = Gravity.CENTER
+        var mTag = ""
+        var mIds: IntArray? = null
+        var mIsCancelable = false
+        var mOrientation = 0
+        var mAnimationRes = R.style.NiceDialogAnim
+        var mDialogView: View? = null
+        var mOnViewClickListener: NiceAlertDialog.OnViewClickListener? = null
+        var mOnBindViewListener: NiceAlertDialog.OnBindViewListener? = null
+        var mOnDismissListener: DialogInterface.OnDismissListener? = null
+        var mOnKeyListener: DialogInterface.OnKeyListener? = null
+
+        var mAdapter: NiceAdapter<*>? = null
+        var mListItemClickListener: NiceAdapter.ItemClickListener? = null
+        var mListRecyclerId = 0
+        var mListOrientation = 0
 
         fun apply(niceDialogController: NiceDialogController?) {
             niceDialogController?.mFragmentTransaction = mFragmentTransaction
@@ -131,9 +172,16 @@ class NiceDialogController private constructor() {
             niceDialogController?.mOnBindViewListener = mOnBindViewListener
             niceDialogController?.mOnDismissListener = mOnDismissListener
             niceDialogController?.mOnKeyListener = mOnKeyListener
-            niceDialogController?.mBackgroundRes =mBackgroundRes
+            niceDialogController?.mBackgroundRes = mBackgroundRes
+
+            niceDialogController?.mAdapter = mAdapter
+            niceDialogController?.mListItemClickListener = mListItemClickListener
+            niceDialogController?.mListOrientation = mListOrientation
+            niceDialogController?.mListRecyclerId = mListRecyclerId
+
         }
 
     }
+
 
 }
