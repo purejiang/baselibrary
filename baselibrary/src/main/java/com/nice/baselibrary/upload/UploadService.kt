228036d1fdd4d1bc3957b6ae0f11b0f71b120867
JiangCodePlus <m18569290458@163.com>
2019-05-19 21:26:54 +0800

    新增upload功能

---
+++ baselibrary/src/main/java/com/nice/baselibrary/upload/UploadService.kt
@@ -0,0 +1,19 @@
+package com.nice.baselibrary.upload
+
+import okhttp3.MultipartBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Url
+
+/**
+ * @author JPlus
+ * @date 2019/5/13.
+ */
+interface UploadService {
+    @Multipart
+    @POST
+    fun uploadFiles(@Part photo:MultipartBody.Part, @Url url:String):Call<ResponseBody>
+}
