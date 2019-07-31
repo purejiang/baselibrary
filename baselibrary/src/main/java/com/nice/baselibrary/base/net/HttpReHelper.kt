89062a782209e740771c0967ab572db3318d6281
JiangCodePlus <m18569290458@163.com>
2019-07-31 15:06:27 +0800

    新增post请求类

---
+++ baselibrary/src/main/java/com/nice/baselibrary/base/net/HttpReHelper.kt
@@ -0,0 +1,31 @@
+package com.nice.baselibrary.base.net
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * @author JPlus
+ * @date 2019/5/13.
+ */
+class HttpReHelper {
+    private var mRetrofit:Retrofit?=null
+
+    constructor(okHttpClient: OkHttpClient,baseUrl:String){
+        mRetrofit = Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+    }
+    /**
+     * 创建新的service
+     * @return
+     */
+    fun getService(): HttpService?{
+        return mRetrofit?.create(HttpService::class.java)
+    }
+}
---
+++ baselibrary/src/main/java/com/nice/baselibrary/base/net/HttpService.kt
@@ -0,0 +1,16 @@
+package com.nice.baselibrary.base.net
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Url
+
+/**
+ * @author JPlus
+ * @date 2019/5/13.
+ */
+interface HttpService {
+    @POST
+    fun sendHttp(@Body any:Any, @Url url:String):Call<ResponseBody>
+}
