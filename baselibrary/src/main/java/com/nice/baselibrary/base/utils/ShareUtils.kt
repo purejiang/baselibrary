6a36deaad541bb0aa4e8f3bf390a1060b8119eb3
JiangCodePlus <m18569290458@163.com>
2019-12-09 17:55:46 +0800

    新增分享功能工具类

---
+++ baselibrary/src/main/java/com/nice/baselibrary/base/utils/ShareUtils.kt
@@ -0,0 +1,25 @@
+package com.nice.baselibrary.base.utils
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.core.content.FileProvider
+import java.io.File
+
+/**
+ * @author JPlus
+ * @date 2019/11/23.
+ */
+object ShareUtils {
+    fun shareFile(activity: Activity, file:File, title:String){
+        val uri = UriUtils.getUriByPath(file.absolutePath, activity)
+        var shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        //其中imgUri为图片的标识符
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+        shareIntent.type = "*/*"
+        //切记需要使用Intent.createChooser，否则会出现别样的应用选择框，您可以试试
+        shareIntent = Intent.createChooser(shareIntent, title)
+        activity.startActivity(shareIntent)
+    }
+}
